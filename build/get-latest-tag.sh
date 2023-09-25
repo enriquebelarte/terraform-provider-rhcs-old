1798d7c6ab799714f4f60cc92f6c0cd1f173e212
Enrique Belarte Luque <ebelarte@redhat.com>
2023-09-25 11:38:20 +0200

    Fix latest REL_VER variable

--- build/get-latest-tag.sh
+++ build/get-latest-tag.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-REL_VER=$(git describe --tags --abbrev=0)
+REL_VER=$(git ls-remote --tags --sort='-v:refname' https://github.com/enriquebelarte/terraform-provider-rhcs | head -n 1 | awk -F/ {'print $3'})
 echo $REL_VER
